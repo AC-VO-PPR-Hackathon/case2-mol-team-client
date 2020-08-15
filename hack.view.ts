c57ffb3d778fbfdb246d6ad7892466644af3b07e
nin-jin <nin-jin@ya.ru>
2020-08-15 19:47:59 +0300

    Скелетон.

--- hack.view.ts
+++ hack.view.ts
@@ -60,14 +60,16 @@ namespace $.$$ {
 			const gestures = this.gestures()
 			
 			const ctx = canvas.getContext('2d')!
-			ctx.clearRect(0,0,600,600)
-			ctx.strokeStyle = 'blue'
-			ctx.beginPath();
-			ctx.moveTo(0,0);
-			ctx.lineTo(600,600);
-			ctx.moveTo(0,600);
-			ctx.lineTo(600,0);
-			ctx.stroke();
+			// ctx.clearRect(0,0,600,600)
+			// ctx.strokeStyle = 'blue'
+			// ctx.beginPath();
+			// ctx.moveTo(0,0);
+			// ctx.lineTo(600,600);
+			// ctx.moveTo(0,600);
+			// ctx.lineTo(600,0);
+			// ctx.stroke();
+
+			draw_hands(ctx, gestures)
 
 			return null
 
@@ -128,7 +130,7 @@ namespace $.$$ {
 
 			const links = this.links()
 
-			const index = links.indexOf( document.activeElement as HTMLElement )
+			const index = links.indexOf( document.activeElement! as HTMLElement )
 			const next = links[ index + 1 ] ?? links[0]
 			
 			console.log( next )
@@ -141,7 +143,7 @@ namespace $.$$ {
 
 			const links = this.links()
 
-			const index = links.indexOf( document.activeElement as HTMLElement )
+			const index = links.indexOf( document.activeElement! as HTMLElement )
 			const next = links[ index - 1 ] ?? links[ links.length - 1 ]
 			
 			console.log( next )
@@ -174,5 +176,89 @@ namespace $.$$ {
 		}
 		
 	}
+
+
+
+
+	function draw_line(ctx:any, dot_1:any, dot_2:any){
+		ctx.strokeStyle = 'blue'
+
+		ctx.beginPath();
+		ctx.moveTo(X(dot_1), Y(dot_1))
+		ctx.lineTo(X(dot_2), Y(dot_2))
+
+		ctx.stroke();
+	}
+
+	function draw_dot(ctx:any, dot:any){
+		ctx.fillStyle = "red"
+
+		ctx.beginPath();
+		ctx.arc(X(dot), Y(dot), 3, 0, Math.PI*2, false)
+		ctx.fill()
+	}
+
+	function draw_all_dot(ctx:any, dots:any[]){
+		for (const dot of dots){
+			draw_dot(ctx, dot)
+		}
+	}
+
+	function draw_hands(ctx:any, predictions:any[]) {
+
+		ctx.clearRect(0,0,600,600)
+		ctx.lineWidth = '10'
+
+		for (const pred of predictions) {
+			const index = pred.annotations.indexFinger
+			const thumb = pred.annotations.thumb
+			const middle = pred.annotations.middleFinger
+			const ringFinger = pred.annotations.ringFinger
+			const pinky = pred.annotations.pinky
+
+			const palm = pred.annotations.palmBase
+
+			draw_all_dot(ctx, palm)
+			draw_line(ctx, palm[0], index[0])
+			draw_line(ctx, palm[0], thumb[0])
+			draw_line(ctx, palm[0], middle[0])
+			draw_line(ctx, palm[0], ringFinger[0])
+			draw_line(ctx, palm[0], pinky[0])
+
+			draw_all_dot(ctx, index)
+			draw_line(ctx, index[0], index[1])
+			draw_line(ctx, index[1], index[2])
+			draw_line(ctx, index[2], index[3])
+
+			draw_all_dot(ctx, thumb)
+			draw_line(ctx, thumb[0], thumb[1])
+			draw_line(ctx, thumb[1], thumb[2])
+			draw_line(ctx, thumb[2], thumb[3])
+
+			draw_all_dot(ctx, middle)
+			draw_line(ctx, middle[0], middle[1])
+			draw_line(ctx, middle[1], middle[2])
+			draw_line(ctx, middle[2], middle[3])
+
+			draw_all_dot(ctx, ringFinger)
+			draw_line(ctx, ringFinger[0], ringFinger[1])
+			draw_line(ctx, ringFinger[1], ringFinger[2])
+			draw_line(ctx, ringFinger[2], ringFinger[3])
+
+			draw_all_dot(ctx, pinky)
+			draw_line(ctx, pinky[0], pinky[1])
+			draw_line(ctx, pinky[1], pinky[2])
+			draw_line(ctx, pinky[2], pinky[3])
+		}
+
+	}
+
+	function X( coords:any ) {
+		return coords[0]
+	} 
+
+	function Y( coords:any ) {
+		return coords[1]
+	} 
 
 }
