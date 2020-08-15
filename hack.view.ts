ec739393a0fd4da17e74ad7f4e48c4e8fb0d5563
nin-jin <nin-jin@ya.ru>
2020-08-15 15:40:21 +0300

    Better speech

--- hack.view.ts
+++ hack.view.ts
@@ -19,15 +19,19 @@ namespace $.$$ {
 		enter() {
 			const focused = $mol_view_selection.focused()[0] as HTMLElement
 			focused.dispatchEvent( new MouseEvent( 'click', { bubbles: true } ) )
+			return true
 		}
 
 		exit() {
 			const query = '[mol_page_head] [mol_icon_cross]'
 			const close = [ ... document.querySelectorAll( query ) ].slice(-1)[0] as HTMLElement
 			close.dispatchEvent( new MouseEvent( 'click', { bubbles: true } ) )
+			return true
 		}
 
 		go( [ topic ] : [ string ] ) {
+
+			topic = topic.replace( /(а|о|у|е|и|ь|)$/ , '' )
 
 			const query = 'a, button'
 			const links = [ ... document.querySelectorAll( query ) ] as HTMLElement[]
@@ -41,8 +45,44 @@ namespace $.$$ {
 				console.log( link )
 				link.dispatchEvent( new MouseEvent( 'click', { bubbles: true } ) )
 
-			}			
+				return true
+			}
 
+		}
+		
+		forward() {
+
+			const query = 'a, button'
+			const links = [ ... document.querySelectorAll( query ) ] as HTMLElement[]
+
+			const index = links.indexOf( document.activeElement as HTMLElement )
+			const next = links[ index + 1 ] ?? links[0]
+			
+			console.log( next )
+			next.focus()
+
+			return true
+		}
+		
+		backward() {
+
+			const query = 'a, button'
+			const links = [ ... document.querySelectorAll( query ) ] as HTMLElement[]
+
+			const index = links.indexOf( document.activeElement as HTMLElement )
+			const next = links[ index - 1 ] ?? links[ links.length - 1 ]
+			
+			console.log( next )
+			next.focus()
+
+			return true
+		}
+		
+		select() {
+
+			document.activeElement?.dispatchEvent( new MouseEvent( 'click', { bubbles: true } ) )
+
+			return true
 		}
 		
 	}
